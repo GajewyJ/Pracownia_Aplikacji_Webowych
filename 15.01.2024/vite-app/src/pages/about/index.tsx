d52915f3e2eda4517cb8fee85f5eeeca8e437de6
GajewyJ <90028969+GajewyJ@users.noreply.github.com>
2024-01-15 13:49:42 +0100

    15.01.2024

---
+++ 15.01.2024/vite-app/src/pages/about/index.tsx
@@ -0,0 +1,9 @@
+import Heading from "../../components/heading"
+
+export default function About(){
+    return(
+        <div>
+            <Heading title={"About"}/>
+        </div>
+    )
+}
---
+++ 15.01.2024/vite-app/src/pages/contact/index.tsx
@@ -0,0 +1,9 @@
+import Heading from "../../components/heading"
+
+export default function Contact(){
+    return(
+        <div>
+            <Heading title={"Contact"}/>
+        </div>
+    )
+}
